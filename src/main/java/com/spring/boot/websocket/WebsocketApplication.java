4ea2369fb838c4df8b44206a6b692f3b605afe46
Lakindu <it21066184@my.sliit.lk>
2023-08-13 23:54:17 +0530

    configuration implemented

--- src/main/java/com/spring/boot/websocket/WebsocketApplication.java
+++ src/main/java/com/spring/boot/websocket/WebsocketApplication.java
@@ -1,13 +1,24 @@
 package com.spring.boot.websocket;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WebsocketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebsocketApplication.class, args);
 	}
 
+	@Value("${server.port}")
+	private int serverPost;
+
+	@GetMapping("/")
+	public String welcomeMessage(){
+		return "Spring boot WebSocket Project Running Well on Port " + serverPost;
+	}
 }
